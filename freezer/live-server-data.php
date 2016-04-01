b0667314f4d116f76e08bca102bbec1320fc0b31
Wayne Ross <mr_trekkie@yahoo.com>
2016-04-01 00:57:33 -0400

    Resolve correct timezone

--- freezer/live-server-data.php
+++ freezer/live-server-data.php
@@ -4,8 +4,11 @@ require_once('../DS1631.php');
 // Set the JSON header
 header("Content-type: text/json");
 
+// how many seconds is my time different from that reported by time()
+$time_offset = timezone_offset_get(timezone_open('America/New_York'),new DateTime());
+
 // The x value is the current JavaScript time, which is the Unix time multiplied by 1000.
-$x = time() * 1000;
+$x = (time() + $time_offset) * 1000;
 // The y value is our temperature
 $ds1631 = new DS1631();
 $y = $ds1631->f();
