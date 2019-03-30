59cda7b197921680330d821d34665a399e383689
Wayne Ross <mr_trekkie@yahoo.com>
2019-03-30 16:19:13 -0400

    Can see all Unique items (folded together based on SAMEAS)

--- hiddenpantry/reportzero.php
+++ hiddenpantry/reportzero.php
@@ -4,8 +4,24 @@ include_once 'db.php';
 include_once 'header.html';
 include_once 'reportheader.html';
 
+$query = urldecode($_GET['all']);
+if ($query)
+  $query="";
+else
+  $query="WHERE total=0";
+
+echo "<center>";
+$handle = fopen("lastbackup.txt", "r");
+if ($handle) {
+  while (($line = fgets($handle)) !== false) {
+    echo "Last backup $line";
+    break; // one and done!
+  }
+  fclose($handle);
+}
+
 echo "<hr>";
-//                          "SELECT (case when sameas='' then upc else sameas end) as same, sum(quant)    quant, brand, descrip, size, flavor, cat FROM inven WHERE quant = '0'");
+
 mysql_query("DROP TEMPORARY TABLE IF EXISTS tally");
 $tt = mysql_query("CREATE TEMPORARY TABLE tally
   SELECT
@@ -18,10 +34,8 @@ $tt = mysql_query("CREATE TEMPORARY TABLE tally
   (SELECT cat     FROM inven WHERE upc = same) AS cat
 FROM inven
 GROUP BY same");
-$deflist = mysql_query("SELECT same, total, brand, descrip, size, flavor, cat FROM tally WHERE total = 0 ORDER BY cat ASC, descrip ASC, brand ASC");
 
-//$deflist=mysql_query("SELECT (case when sameas = '' then upc else sameas end) as same, sum(quant) quant, brand, descrip, size, flavor, cat FROM inven WHERE quant = '0' GROUP BY same ORDER BY cat ASC, descrip ASC, brand ASC");
-
+$deflist = mysql_query("SELECT same, total, brand, descrip, size, flavor, cat FROM tally ".$query." ORDER BY cat ASC, descrip ASC, brand ASC");
 
 while ($all = mysql_fetch_array($deflist)) {
    $results[$all['cat']][] = array ('upc' => $all['same'], 'quant' => $all['total'], 'brand' => $all['brand'], 'descrip' => $all['descrip'], 'size' => $all['size'], 'flavor' => $all['flavor']);
@@ -42,20 +56,19 @@ foreach ($results as $catName => $catData)
       <TR><TD>
 		<b><font face=arial size=2 color=white>'.$catName.'</b><br/></font></td></tr></table></center>'."\n");
    foreach ($catData as $itemNum => $itemData)
-   
    {
+      if ($query=="" && $itemData['quant']==0)
+        $color="red";
+      else
+        $color="white";
 error_reporting(1);
-      // if you want to access the row data in this loop, use the following method:
       print('<center><TABLE id=AutoNumber21 style="BORDER-COLLAPSE: collapse" borderColor=#000000 
-      height=12 cellSpacing=3 cellPadding=3 width=600 border=0>
+      bgcolor='.$color.' height=12 cellSpacing=3 cellPadding=3 width=600 border=0>
       <TBODY>
       <TR><TD><font face=arial size=2>
 	<A HREF="editproduct.php?upc='.$itemData['upc'].'">edit</A> - ' .
       $itemData['descrip'].', '.$itemData['brand'].', '.$itemData['size'].',
       '.$itemData['flavor'].' - '.$itemData['quant'].'<br/></td></tr></table></center></font>'."\n"); 
-      // etc. (you must code the field names in hard coded this way)
-      //foreach ($itemData as $fieldName => $value)
-      
    }
 }
 echo '</td></tr></table></center>';
